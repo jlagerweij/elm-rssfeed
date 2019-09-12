c85d939015804d7e425a75a52933f982e0ea1065
Jos Lagerweij <jos@lagerwey.net>
2019-09-12 08:14:01 +0200

    Better task arrangement in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,16 @@ configure<NodeExtension> {
 }
 
 tasks {
-  create<NpmTask>("webpack") {
+  create<NodeTask>("localDev") {
     dependsOn("npmInstall")
-    setArgs(listOf("run", "prod"))
-  }
-  create<NodeTask>("webpackDevServer") {
-    dependsOn("npmInstall")
+    description = "Start local development server on port 8080."
     setScript(file("node_modules/webpack-dev-server/bin/webpack-dev-server"))
     setArgs(listOf("--hot"))
   }
-  create<Copy>("dist") {
+  create<Copy>("assemble") {
     dependsOn("webpack")
+    description = "Assembles the outputs of this project."
+    group = "build"
     from("build/web/static") {
       into("static")
     }
@@ -33,29 +32,41 @@ tasks {
     into("build/dist")
   }
   create("clean") {
+    group = "build"
+    description = "Deletes the build, node_modules and elm-stuff directories."
     doLast {
       delete("node_modules")
       delete("elm-stuff")
       delete("build")
     }
   }
-  create("build") {
-    dependsOn("webpack")
+  create<NpmTask>("webpack") {
+    dependsOn("npmInstall")
+    description = "Assembles this project."
+    group = "build"
+    setArgs(listOf("run", "prod"))
   }
-  create<Exec>("deployStatic") {
-    dependsOn("dist")
-    commandLine = "scp -r build/dist/static ernie:/volume1/web/j/rss/elm/".split(" ")
-  }
-  create<Exec>("deployHtml") {
-    dependsOn("dist")
-    commandLine = "scp -r build/dist/index.html ernie:/volume1/web/j/rss/elm/".split(" ")
-  }
-  create<Exec>("deployConvertScript") {
-    dependsOn("dist")
-    commandLine = "scp -r build/dist/convert-rss-to-json.php ernie:/volume1/web/j/rss/elm/".split(" ")
-  }
-  create("deploy") {
-    dependsOn(listOf("deployStatic", "deployHtml", "deployConvertScript"))
+  create("publish") {
+    dependsOn("assemble")
+    description = "Publish output of this project to a server."
+    group = "build"
+    doLast {
+      exec {
+        println("Publish static")
+        description = "Publish static"
+        commandLine = "scp -r build/dist/static ernie:/volume1/web/j/rss/elm/".split(" ")
+      }
+      exec {
+        println("Publish HTML")
+        description = "Publish HTML"
+        commandLine = "scp -r build/dist/index.html ernie:/volume1/web/j/rss/elm/".split(" ")
+      }
+      exec {
+        println("Publish Convert script")
+        description = "Publish Convert script"
+        commandLine = "scp -r build/dist/convert-rss-to-json.php ernie:/volume1/web/j/rss/elm/".split(" ")
+      }
+    }
   }
 }
 
