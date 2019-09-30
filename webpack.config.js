c1bcbba3d69775099977f2c9c18d990ffb336bc0
Jos Lagerweij <j.lagerweij@avisi.nl>
2019-09-30 09:09:39 +0200

    Add favicon.ico

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ const common = {
     new HTMLWebpackPlugin({
       // Use this template to get basic responsive meta tags
       template: "src/static/index.html",
+      favicon: 'src/static/favicon.ico',
       // inject details of output file at end of body
       inject: "body"
     })
