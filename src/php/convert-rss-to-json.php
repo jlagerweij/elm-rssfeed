5e6bfb99b12d00500067708fad5ecc78fdb46127
Jos Lagerweij <j.lagerweij@avisi.nl>
2019-10-19 12:26:59 +0200

    Rename docker containers

--- src/php/convert-rss-to-json.php
+++ src/php/convert-rss-to-json.php
@@ -1,6 +1,6 @@
 <?php
 
-$feedConfigs = json_decode(file_get_contents('http://elm-rssfeed/api/feeds.json'));
+$feedConfigs = json_decode(file_get_contents('http://web/api/feeds.json'));
 
 class Item
 {
